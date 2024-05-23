f11413607b8d0983a38bdfb30971284635cdd595
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-05-23 10:31:26 -0400

    Added hash update note
    PROD-10894

--- versions.mk
+++ versions.mk
@@ -1,3 +1,5 @@
+# After updating versions file, run tools/calc-hashes.sh script
+
 SUMMIT_60_RADIO_STACK_VERSION_VALUE      = 0.0.0.0
 SUMMIT_BDSDMAC_RADIO_STACK_VERSION_VALUE = 0.0.0.0
 SUMMIT_LWB_RADIO_STACK_VERSION_VALUE     = 0.0.0.0
