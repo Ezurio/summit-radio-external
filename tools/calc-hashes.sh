a0182f08fe5b922c52c36165fe64ce1ba5455b51
Dean Ramsier <dean.ramsier@ezurio.com>
2025-04-30 16:08:44 -0400

    Remove LWB5 support
    PROD-26780

--- tools/calc-hashes.sh
+++ tools/calc-hashes.sh
@@ -146,7 +146,6 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 # Calculate hashes for the summit-firmware-lwb package
 {
 	calc_file "firmware/${version_lwb}/summit-lwb-firmware-${version_lwb}.tar.bz2"
-	calc_file "firmware/${version_lwb}/summit-lwb5-firmware-${version_lwb}.tar.bz2"
 	calc_file "firmware/${version_lwb}/summit-lwbplus-firmware-${version_lwb}.tar.bz2"
 
 	for i in sdio-div sdio-sa sdio-sa-m2 usb-div usb-sa usb-sa-m2
