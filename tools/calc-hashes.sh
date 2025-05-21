4c21405a164a4deaa551f075a455cfd851bdb7ef
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-05-21 19:11:50 -0400

    Combine radio stacks versions
    PROD-27490

--- tools/calc-hashes.sh
+++ tools/calc-hashes.sh
@@ -6,10 +6,6 @@ err_report() {
     [ $? -eq 0 ] || echo "Error calculating hashes" > /dev/stderr
 }
 
-get_version() {
-	sed -rn "s,.*${1}.*=\s*([0-9.]+),\1,p" versions.mk
-}
-
 prefix="/var/www/html/builds/linux"
 cmd="ssh -o ControlMaster=auto -o ControlPersist=5s -o ControlPath=/tmp/ssh-fileshare fileshare@files.devops.rfpros.com"
 
@@ -18,35 +14,21 @@ calc_hash() {
     sed -r "s/([0-9a-f]+)  .*\/(.*-[0-9.]+\.tar.*)/sha256  \1  \2/"
 }
 
-unique() {
-	echo "$@" | tr ' ' '\n' | sort -u | tr '\n' ' '
-}
-
 hash_file() {
 	echo "package/${1}/${1}.hash"
 }
 
-if [ -n "${1}" ]; then
-	sed -i -r "s/(.+=).*/\1 ${1}/g" versions.mk
-fi
+version=${1}
+sed -i -r "s/(.+=).*/\1 ${1}/g" versions.mk
 
 LICENSE_SUMMIT='sha256  ff126d9b0f7f474b2652064d045c6b25a015eb94f9d0ac29c96d053c94577343  LICENSE.ezurio'
-
-version_60=$(get_version "60")
-version_bdsdmac=$(get_version "BDSDMAC")
-version_lwb=$(get_version "LWB")
-version_msd=$(get_version "MSD")
-version_nx=$(get_version "NX")
-version_ti=$(get_version "TI")
-
-version_all=$(unique "${version_60}" "${version_bdsdmac}" "${version_lwb}" "${version_msd}" "${version_nx}" "${version_ti}")
 
 # Calculate hashes for the summit-adaptive_ww package
 {
 	files=
 	for i in x86 x86_64 arm-eabi arm-eabihf aarch64 powerpc64-e5500
 	do
-		files="${files} adaptive_ww/laird/${version_60}/adaptive_ww-${i}-${version_60}.tar.bz2"
+		files="${files} adaptive_ww/laird/${version}/adaptive_ww-${i}-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -55,8 +37,7 @@ version_all=$(unique "${version_60}" "${version_bdsdmac}" "${version_lwb}" "${ve
 
 # Calculate hashes for the summit-adaptive_bt package
 {
-	files=
-	files="${files} adaptive_bt/src/${version_60}/adaptive_bt-src-${version_60}.tar.gz"
+	files="adaptive_bt/src/${version}/adaptive_bt-src-${version}.tar.gz"
 	calc_hash "${files}"
 
 	echo "${LICENSE_SUMMIT}"
@@ -67,19 +48,12 @@ version_all=$(unique "${version_60}" "${version_bdsdmac}" "${version_lwb}" "${ve
 	files=
 	for i in x86 x86_64 arm-eabi arm-eabihf aarch64 powerpc64-e5500
 	do
-		files="${files} summit_supplicant/laird/${version_60}/summit_supplicant_libs-${i}-${version_60}.tar.bz2"
+		files="${files} summit_supplicant/laird/${version}/summit_supplicant_libs-${i}-${version}.tar.bz2"
 	done
-
-	if [ "${version_msd}" != "${version_60}" ]; then
-		for i in arm-eabi arm-eabihf
-		do
-			files="${files} summit_supplicant/laird/${version_msd}/summit_supplicant_libs-${i}-${version_msd}.tar.bz2"
-		done
-	fi
 
 	for i in arm-eabi arm-eabihf
 	do
-		files="${files} summit_supplicant/laird/${version_msd}/summit_supplicant_libs_legacy-${i}-${version_msd}.tar.bz2"
+		files="${files} summit_supplicant/laird/${version}/summit_supplicant_libs_legacy-${i}-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -88,11 +62,7 @@ version_all=$(unique "${version_60}" "${version_bdsdmac}" "${version_lwb}" "${ve
 
 # Calculate hashes for the summit-supplicant package
 {
-	files=
-	for v in ${version_all}
-	do
-		files="${files}summit_supplicant/laird/${v}/summit_supplicant-src-${v}.tar.gz"
-	done
+	files="summit_supplicant/laird/${version}/summit_supplicant-src-${version}.tar.gz"
 	calc_hash "${files}"
 
 	echo "sha256  f1b5992bbdd015c3ccb7faaadd62ef58ed821e15b9329bf2ceb27511ccc3f562  README"
@@ -104,11 +74,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 
 # Calculate hashes for the summit-linux-backports package
 {
-	files=
-	for v in ${version_all}
-	do
-		files="${files} backports/laird/${v}/summit-backports-${v}.tar.bz2"
-	done
+	files="backports/laird/${version}/summit-backports-${version}.tar.bz2"
 	calc_hash "${files}"
 
 	echo "sha256  fb5a425bd3b3cd6071a3a9aff9909a859e7c1158d54d32e07658398cd67eb6a0  COPYING"
@@ -119,11 +85,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 
 # Calculate hashes for the summit-network-manager package
 {
-	files=
-	for v in ${version_all}
-	do
-		files="${files} lrd-network-manager/src/${v}/summit-network-manager-src-${v}.tar.xz"
-	done
+	files="lrd-network-manager/src/${version}/summit-network-manager-src-${version}.tar.xz"
 	calc_hash "${files}"
 
 	echo "sha256  8177f97513213526df2cf6184d8ff986c675afb514d4e68a404010521b880643  COPYING"
@@ -137,9 +99,9 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in pcie-uart pcie-usb sdio-uart sdio-sdio usb-usb usb-uart
 	do
-		files="${files} firmware/${version_60}/summit-60-radio-firmware-${i}-${version_60}.tar.bz2"
+		files="${files} firmware/${version}/summit-60-radio-firmware-${i}-${version}.tar.bz2"
 	done
-	files="${files} firmware/${version_60}/summit-som8mp-radio-firmware-${version_60}.tar.bz2"
+	files="${files} firmware/${version}/summit-som8mp-radio-firmware-${version}.tar.bz2"
 	calc_hash "${files}"
 
 	echo "sha256  2accdbff2dfad766f2533a8976f15f550625253987b6ee75c06f80a8227822c2  LICENSE.nxp1"
@@ -148,8 +110,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 
 # Calculate hashes for the summit-firmware-bdsdmac package
 {
-	files=
-	files="${files} firmware/${version_bdsdmac}/summit-bdsdmac-firmware-${version_bdsdmac}.tar.bz2"
+	files="firmware/${version}/summit-bdsdmac-firmware-${version}.tar.bz2"
 	calc_hash "${files}"
 
 	echo "sha256  4ea56b251222f9d121c22f92e5d860750ab1b29a1d743be83b0f3af311d4972c  LICENSE.qca_firmware"
@@ -158,22 +119,22 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 # Calculate hashes for the summit-firmware-lwb package
 {
 	files=
-	files="${files} firmware/${version_lwb}/summit-lwb-firmware-${version_lwb}.tar.bz2"
-	files="${files} firmware/${version_lwb}/summit-lwbplus-firmware-${version_lwb}.tar.bz2"
+	files="${files} firmware/${version}/summit-lwb-firmware-${version}.tar.bz2"
+	files="${files} firmware/${version}/summit-lwbplus-firmware-${version}.tar.bz2"
 
 	for i in sdio-div sdio-sa sdio-sa-m2 usb-div usb-sa usb-sa-m2
 	do
-		files="${files} firmware/${version_lwb}/summit-lwb5plus-${i}-firmware-${version_lwb}.tar.bz2"
+		files="${files} firmware/${version}/summit-lwb5plus-${i}-firmware-${version}.tar.bz2"
 	done
 
 	for i in sdio pcie
 	do
-		files="${files} firmware/${version_lwb}/summit-if573-${i}-firmware-${version_lwb}.tar.bz2"
+		files="${files} firmware/${version}/summit-if573-${i}-firmware-${version}.tar.bz2"
 	done
 
 	for i in sdio-div sdio-sa
 	do
-		files="${files} firmware/${version_lwb}/summit-if513-${i}-firmware-${version_lwb}.tar.bz2"
+		files="${files} firmware/${version}/summit-if513-${i}-firmware-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -186,7 +147,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in 6003 6004
 	do
-		files="${files} firmware/${version_msd}/summit-ath6k-${i}-firmware-${version_msd}.tar.bz2"
+		files="${files} firmware/${version}/summit-ath6k-${i}-firmware-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -198,8 +159,8 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 # Calculate hashes for the summit-firmware-nx packages
 {
 	files=
-	files="${files} firmware/${version_nx}/summit-nx61x-firmware-${version_nx}.tar.bz2"
-	files="${files} firmware/${version_nx}/summit-nx61x-1218-firmware-${version_nx}.tar.bz2"
+	files="${files} firmware/${version}/summit-nx61x-firmware-${version}.tar.bz2"
+	files="${files} firmware/${version}/summit-nx61x-1218-firmware-${version}.tar.bz2"
 	calc_hash "${files}"
 
 	echo "sha256  3dd8aa2ede25fcc34b72754473dc3d3924a57b550bfcafe3a48d8bd951abf383  LICENSE.nxp2"
@@ -211,7 +172,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in WW US JP EU CA AU
 	do
-		files="${files} firmware/${version_ti}/summit-ti351-${i}-firmware-${version_ti}.tar.bz2"
+		files="${files} firmware/${version}/summit-ti351-${i}-firmware-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -223,7 +184,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in x86 x86_64 arm-eabi arm-eabihf aarch64 powerpc64-e5500
 	do
-		files="${files} mfg60n/laird/${version_60}/mfg60n-${i}-${version_60}.tar.bz2"
+		files="${files} mfg60n/laird/${version}/mfg60n-${i}-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -235,7 +196,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in x86 x86_64 arm-eabi arm-eabihf aarch64 powerpc64-e5500
 	do
-		files="${files} mfg611/laird/${version_nx}/mfg611-${i}-${version_nx}.tar.bz2"
+		files="${files} mfg611/laird/${version}/mfg611-${i}-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -247,7 +208,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in arm-eabi arm-eabihf
 	do
-		files="${files} reg45n/laird/${version_msd}/reg45n-${i}-${version_msd}.tar.bz2"
+		files="${files} reg45n/laird/${version}/reg45n-${i}-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -259,7 +220,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in arm-eabi arm-eabihf
 	do
-		files="${files} reg50n/laird/${version_msd}/reg50n-${i}-${version_msd}.tar.bz2"
+		files="${files} reg50n/laird/${version}/reg50n-${i}-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -271,7 +232,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in arm-eabi arm-eabihf aarch64
 	do
-		files="${files} regCypress/laird/${version_lwb}/regCypress-${i}-${version_lwb}.tar.bz2"
+		files="${files} regCypress/laird/${version}/regCypress-${i}-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -284,7 +245,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in x86 x86_64 arm-eabi arm-eabihf aarch64 powerpc64-e5500
 	do
-		files="${files} regLWB5plus/laird/${version_lwb}/regLWB5plus-${i}-${version_lwb}.tar.bz2"
+		files="${files} regLWB5plus/laird/${version}/regLWB5plus-${i}-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -297,7 +258,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in x86 x86_64 arm-eabi arm-eabihf aarch64 powerpc64-e5500
 	do
-		files="${files} regLWBplus/laird/${version_lwb}/regLWBplus-${i}-${version_lwb}.tar.bz2"
+		files="${files} regLWBplus/laird/${version}/regLWBplus-${i}-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -310,7 +271,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in x86 x86_64 arm-eabi arm-eabihf aarch64 powerpc64-e5500
 	do
-		files="${files} regIF573/laird/${version_lwb}/regIF573-${i}-${version_lwb}.tar.bz2"
+		files="${files} regIF573/laird/${version}/regIF573-${i}-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -323,7 +284,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in x86 x86_64 arm-eabi arm-eabihf aarch64 powerpc64-e5500
 	do
-		files="${files} regIF513/laird/${version_lwb}/regIF513-${i}-${version_lwb}.tar.bz2"
+		files="${files} regIF513/laird/${version}/regIF513-${i}-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
@@ -335,7 +296,7 @@ cp -f "$(hash_file summit-supplicant)" "$(hash_file summit-hostapd)"
 	files=
 	for i in x86 x86_64 arm-eabi arm-eabihf aarch64 powerpc64-e5500
 	do
-		files="${files} regTI351/laird/${version_ti}/regTI351-${i}-${version_ti}.tar.bz2"
+		files="${files} regTI351/laird/${version}/regTI351-${i}-${version}.tar.bz2"
 	done
 	calc_hash "${files}"
 
