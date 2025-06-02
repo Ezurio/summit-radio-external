0655da1cbda7369d689bc6ebe0683e64f07ea0fe
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-06-02 17:09:23 -0400

    nx61x: Added serdev support, fixed installation

--- package/summit-firmware-nx/summit-firmware-nx.mk
+++ package/summit-firmware-nx/summit-firmware-nx.mk
@@ -1,7 +1,8 @@
 ifneq ($(BR2_LRD_DEVEL_BUILD),y)
 
-SUMMIT_FIRMWARE_NX_VERSION = $(SUMMIT_NX_RADIO_STACK_VERSION_VALUE)
-SUMMIT_FIRMWARE_NX_SOURCE = summit-nx61x-firmware-$(SUMMIT_FIRMWARE_NX_VERSION).tar.bz2
+SUMMIT_FIRMWARE_NX_VERSION = $(SUMMIT_RADIO_STACK_VERSION_VALUE)
+SUMMIT_FIRMWARE_NX_SOURCE = $(firstword $(SUMMIT_FIRMWARE_NX_DOWNLOADS))
+SUMMIT_FIRMWARE_NX_EXTRA_DOWNLOADS = $(filter-out $(SUMMIT_FIRMWARE_NX_SOURCE),$(SUMMIT_FIRMWARE_NX_DOWNLOADS))
 SUMMIT_FIRMWARE_NX_STRIP_COMPONENTS = 0
 SUMMIT_FIRMWARE_NX_LICENSE = NXP, Ezurio
 SUMMIT_FIRMWARE_NX_LICENSE_FILES = LICENSE.nxp2 LICENSE.ezurio
@@ -16,11 +17,34 @@ else
   SUMMIT_FIRMWARE_NX_SITE = $(SUMMIT_RADIO_URI_BASE_NX)-$(SUMMIT_FIRMWARE_NX_VERSION)
 endif
 
+ifeq ($(BR2_PACKAGE_SUMMIT_FIRMWARE_NX61X),y)
+  SUMMIT_FIRMWARE_NX_DOWNLOADS += summit-nx61x-firmware-$(SUMMIT_FIRMWARE_NX_VERSION).tar.bz2
+endif
+
+ifeq ($(BR2_PACKAGE_SUMMIT_FIRMWARE_NX61X_SERDEV),y)
 define SUMMIT_FIRMWARE_NX_INSTALL_TARGET_CMDS
-  rsync -rlpDWK --no-perms --inplace $(@D)/lib $(TARGET_DIR)
+  $(INSTALL) -D -m 0644 -t $(TARGET_DIR)/lib/firmware/nxp \
+    $(@D)/lib/firmware/nxp/sd_* \
+    $(@D)/lib/firmware/nxp/uart* \
+    $(@D)/lib/firmware/nxp/*rgpower* \
+    $(@D)/lib/firmware/nxp/wifi_prod_serdev_params.conf
+
   $(INSTALL) -d $(TARGET_DIR)/etc/modprobe.d
-  echo "options moal mod_para=nxp/wifi_mod_para.conf" > $(TARGET_DIR)/etc/modprobe.d/moal.conf
+  echo "options moal mod_para=nxp/wifi_prod_serdev_params.conf" > \
+    $(TARGET_DIR)/etc/modprobe.d/moal.conf
 endef
+else
+define SUMMIT_FIRMWARE_NX_INSTALL_TARGET_CMDS
+  $(INSTALL) -D -m 0644 -t $(TARGET_DIR)/lib/firmware/nxp \
+    $(@D)/lib/firmware/nxp/sduart_* \
+    $(@D)/lib/firmware/nxp/*rgpower* \
+    $(@D)/lib/firmware/nxp/wifi_prod_params.conf
+
+  $(INSTALL) -d $(TARGET_DIR)/etc/modprobe.d
+  echo "options moal mod_para=nxp/wifi_prod_params.conf" > \
+    $(TARGET_DIR)/etc/modprobe.d/moal.conf
+endef
+endif
 
 endif
 
