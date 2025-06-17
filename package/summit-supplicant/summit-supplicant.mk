47a7d7f6b9c0a95da45941a599f0e00486fde66f
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-06-17 10:33:02 -0400

    summit-supplicant: Improved selection granularity
    PROD-27490

--- package/summit-supplicant/summit-supplicant.mk
+++ package/summit-supplicant/summit-supplicant.mk
@@ -48,15 +48,7 @@ SUMMIT_SUPPLICANT_DEPENDENCIES += summit-supplicant-libs
 endif
 
 ifeq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_FIPS),y)
-SUMMIT_SUPPLICANT_FIPS = CONFIG_FIPS=y
-SUMMIT_SUPPLICANT_FIPS += CONFIG_FIPS_SUMMIT=y
-endif
-
-ifeq ($(BR2_PACKAGE_LIBOPENSSL_3_0),y)
-ifeq ($(BR2_PACKAGE_LIBOPENSSL_ENABLE_FIPS),y)
-SUMMIT_SUPPLICANT_FIPS = CONFIG_FIPS=y
-SUMMIT_SUPPLICANT_FIPS += CONFIG_FIPS_SUMMIT=y
-endif
+SUMMIT_SUPPLICANT_FIPS = CONFIG_FIPS=y CONFIG_FIPS_SUMMIT=y
 endif
 
 ifeq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_LEGACY),y)
@@ -65,56 +57,33 @@ ifeq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_LIBS_BUILD),y)
 endif
     SUMMIT_SUPPLICANT_CONFIG = $(@D)/wpa_supplicant/config_legacy
 else
-    SUMMIT_SUPPLICANT_DEPENDENCIES += dbus
     SUMMIT_SUPPLICANT_CONFIG = $(@D)/wpa_supplicant/config_openssl
 endif
 
 define SUMMIT_SUPPLICANT_BUILD_CMDS
 	cp $(SUMMIT_SUPPLICANT_CONFIG) $(@D)/wpa_supplicant/.config
-	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/wpa_supplicant BINDIR=/usr/sbin $(SUMMIT_SUPPLICANT_FIPS)
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) \
+		$(MAKE) -C $(@D)/wpa_supplicant BINDIR=/usr/sbin $(SUMMIT_SUPPLICANT_FIPS)
 	$(TARGET_OBJCOPY) $(@D)/wpa_supplicant/wpa_supplicant $(@D)/wpa_supplicant/sdcsupp
 endef
 
-ifeq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_WPA_CLI),y)
-define SUMMIT_SUPPLICANT_INSTALL_WPA_CLI
+ifeq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_CLI),y)
+define SUMMIT_SUPPLICANT_INSTALL_CLI
 	$(INSTALL) -D -m 755 -t $(TARGET_DIR)/usr/sbin $(@D)/wpa_supplicant/wpa_cli
 endef
 endif
 
-ifeq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_WPA_PASSPHRASE),y)
-define SUMMIT_SUPPLICANT_INSTALL_WPA_PASSPHRASE
-	$(INSTALL) -D -m 755 -t $(TARGET_DIR)/usr/sbin $(@D)/wpa_supplicant/wpa_passphrase
+ifeq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_PASSPHRASE),y)
+define SUMMIT_SUPPLICANT_INSTALL_PASSPHRASE
+	$(INSTALL) -D -m 755 -t $(TARGET_DIR)/usr/sbin \
+		$(@D)/wpa_supplicant/wpa_passphrase
 endef
 endif
 
-ifneq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_LEGACY),y)
-
-ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT),y)
-    # both wpa_supplicant and sdcsupp installed, postfix to avoid collision
-    SUMMIT_SUPPLICANT_DBUS_SERVICE_POSTFIX = .summit
-else
-    # only sdcsupp installed, no postfix needed
-    SUMMIT_SUPPLICANT_DBUS_SERVICE_POSTFIX =
-endif
-
-ifneq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_DBUS_P2P_ENABLED),y)
-define SUMMIT_SUPPLICANT_INSTALL_DBUS_OVERLAY_CONF
-	mkdir -p $(TARGET_DIR)/etc/wpa_supplicant
-	@echo "p2p_disabled=1" > $(TARGET_DIR)/etc/wpa_supplicant/dbus_overlay.conf
-endef
-endif
-
-define SUMMIT_SUPPLICANT_INSTALL_DBUS
-	$(INSTALL) -m 0644 -D $(@D)/wpa_supplicant/dbus/dbus-wpa_supplicant.conf \
-		$(TARGET_DIR)/etc/dbus-1/system.d/wpa_supplicant.conf
-	$(INSTALL) -m 0644 -D $(@D)/wpa_supplicant/dbus/fi.w1.wpa_supplicant1.service \
-		$(TARGET_DIR)/usr/share/dbus-1/system-services/fi.w1.wpa_supplicant1.service$(SUMMIT_SUPPLICANT_DBUS_SERVICE_POSTFIX)
-	$(SUMMIT_SUPPLICANT_INSTALL_DBUS_OVERLAY_CONF)
-endef
-
+ifeq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_WPA_CLIENT_SO),y)
 define SUMMIT_SUPPLICANT_INSTALL_WPA_CLIENT_SO
-	$(INSTALL) -m 0644 -D $(@D)/$(WPA_SUPPLICANT_SUBDIR)/libwpa_client.so \
-		$(TARGET_DIR)/usr/lib/libwpa_client.so
+	$(INSTALL) -m 0644 -D -t $(TARGET_DIR)/usr/lib \
+		$(@D)/$(WPA_SUPPLICANT_SUBDIR)/libwpa_client.so
 endef
 
 define SUMMIT_SUPPLICANT_INSTALL_STAGING_WPA_CLIENT_SO
@@ -123,7 +92,6 @@ define SUMMIT_SUPPLICANT_INSTALL_STAGING_WPA_CLIENT_SO
 	$(INSTALL) -m 0644 -D -t $(STAGING_DIR)/usr/include \
 		$(@D)/src/common/wpa_ctrl.h
 endef
-
 endif
 
 ifeq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_LIBS_BUILD),y)
@@ -138,6 +106,25 @@ define SUMMIT_SUPPLICANT_INSTALL_STAGING_SUMMIT_SUPPLICANT_SO
 endef
 endif
 
+ifneq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_DBUS_P2P_ENABLED),y)
+define SUMMIT_SUPPLICANT_INSTALL_DBUS_OVERLAY_CONF
+	mkdir -p $(TARGET_DIR)/etc/wpa_supplicant
+	@echo "p2p_disabled=1" > $(TARGET_DIR)/etc/wpa_supplicant/dbus_overlay.conf
+endef
+endif
+
+ifeq ($(BR2_PACKAGE_SUMMIT_SUPPLICANT_DBUS),y)
+SUMMIT_SUPPLICANT_DEPENDENCIES += dbus
+
+define SUMMIT_SUPPLICANT_INSTALL_DBUS
+	$(INSTALL) -m 0644 -D $(@D)/wpa_supplicant/dbus/dbus-wpa_supplicant.conf \
+		$(TARGET_DIR)/etc/dbus-1/system.d/wpa_supplicant.conf
+	$(INSTALL) -m 0644 -D $(@D)/wpa_supplicant/dbus/fi.w1.wpa_supplicant1.service \
+		$(TARGET_DIR)/usr/share/dbus-1/system-services/fi.w1.wpa_supplicant1.service$(if $(BR2_PACKAGE_WPA_SUPPLICANT),.summit,)
+	$(SUMMIT_SUPPLICANT_INSTALL_DBUS_OVERLAY_CONF)
+endef
+endif
+
 define SUMMIT_SUPPLICANT_INSTALL_STAGING_CMDS
 	$(SUMMIT_SUPPLICANT_INSTALL_STAGING_WPA_CLIENT_SO)
 	$(SUMMIT_SUPPLICANT_INSTALL_STAGING_SUMMIT_SUPPLICANT_SO)
@@ -145,24 +132,18 @@ endef
 
 define SUMMIT_SUPPLICANT_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 -t $(TARGET_DIR)/usr/sbin $(@D)/wpa_supplicant/sdcsupp
-	$(SUMMIT_SUPPLICANT_INSTALL_WPA_CLI)
-	$(SUMMIT_SUPPLICANT_INSTALL_WPA_PASSPHRASE)
+	$(SUMMIT_SUPPLICANT_INSTALL_CLI)
+	$(SUMMIT_SUPPLICANT_INSTALL_PASSPHRASE)
 	$(SUMMIT_SUPPLICANT_INSTALL_DBUS)
 	$(SUMMIT_SUPPLICANT_INSTALL_WPA_CLIENT_SO)
 	$(SUMMIT_SUPPLICANT_INSTALL_SUMMIT_SUPPLICANT_SO)
 endef
 
 define SUMMIT_SUPPLICANT_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -m 0644 -D $(@D)/wpa_supplicant/systemd/wpa_supplicant.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/wpa_supplicant.service
-	$(INSTALL) -m 0644 -D $(@D)/wpa_supplicant/systemd/wpa_supplicant@.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/wpa_supplicant@.service
-	$(INSTALL) -m 0644 -D $(@D)/wpa_supplicant/systemd/wpa_supplicant-nl80211@.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/wpa_supplicant-nl80211@.service
-	$(INSTALL) -m 0644 -D $(@D)/wpa_supplicant/systemd/wpa_supplicant-wired@.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/wpa_supplicant-wired@.service
-	$(INSTALL) -m 0644 -D $(SUMMIT_SUPPLICANT_PKGDIR)/50-wpa_supplicant.preset \
-		$(TARGET_DIR)/usr/lib/systemd/system-preset/50-wpa_supplicant.preset
+	$(INSTALL) -m 0644 -D -t $(TARGET_DIR)/usr/lib/systemd/system \
+		$(@D)/wpa_supplicant/systemd/wpa_supplicant*.service \
+	$(INSTALL) -m 0644 -D -t $(TARGET_DIR)/usr/lib/systemd/system-preset \
+		$(SUMMIT_SUPPLICANT_PKGDIR)/50-wpa_supplicant.preset
 endef
 
 $(eval $(generic-package))
