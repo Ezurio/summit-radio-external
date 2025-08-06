a0b1b5fbc7a473795bc13b22a7f326283b039732
Dean Ramsier <dean.ramsier@ezurio.com>
2025-08-06 10:00:34 -0400

    Update to combined radio stack location
    PROD-27490

--- external.mk
+++ external.mk
@@ -4,11 +4,11 @@ SUMMIT_RADIO_STACK_ARCH = $(call qstrip,$(BR2_PACKAGE_SUMMIT_RADIO_STACK_ARCH))
 
 RFPROS_FILESHARE_AUTH ?= $(if $(RFPROS_FILESHARE_USER),$(RFPROS_FILESHARE_USER):$(RFPROS_FILESHARE_PASS)@,)
 
-SUMMIT_RADIO_URI_BASE_60       = https://github.com/Ezurio/Sterling-60-Release-Packages/releases/download/LRD-REL
-SUMMIT_RADIO_URI_BASE_BDSDMAC  = https://github.com/Ezurio/BDSDMAC-Release-Packages/releases/download/LRD-REL
-SUMMIT_RADIO_URI_BASE_LWB      = https://github.com/Ezurio/SonaIF-Release-Packages/releases/download/LRD-REL
-SUMMIT_RADIO_URI_BASE_NX       = https://github.com/Ezurio/SonaNX-Release-Packages/releases/download/LRD-REL
-SUMMIT_RADIO_URI_BASE_TI       = https://github.com/Ezurio/SonaTI-Release-Packages/releases/download/LRD-REL
+SUMMIT_RADIO_URI_BASE_60       = https://github.com/Ezurio/Connectivity_Stack_Release_Packages/releases/download/LRD-REL
+SUMMIT_RADIO_URI_BASE_BDSDMAC  = https://github.com/Ezurio/Connectivity_Stack_Release_Packages/releases/download/LRD-REL
+SUMMIT_RADIO_URI_BASE_LWB      = https://github.com/Ezurio/Connectivity_Stack_Release_Packages/releases/download/LRD-REL
+SUMMIT_RADIO_URI_BASE_NX       = https://github.com/Ezurio/Connectivity_Stack_Release_Packages/releases/download/LRD-REL
+SUMMIT_RADIO_URI_BASE_TI       = https://github.com/Ezurio/Connectivity_Stack_Release_Packages/releases/download/LRD-REL
 SUMMIT_RADIO_URI_BASE_INTERNAL = https://$(RFPROS_FILESHARE_AUTH)files.devops.rfpros.com/builds/linux
 
 ifeq ($(SUMMIT_SOM_URI_BASE_ARCHIVE),)
