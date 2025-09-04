2c58ae61470eedccb8e97cf0c77c049c3db51682
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-09-04 18:08:56 -0400

    Fixed radio uri for som builds

--- external.mk
+++ external.mk
@@ -7,8 +7,4 @@ RFPROS_FILESHARE_AUTH ?= $(if $(RFPROS_FILESHARE_USER),$(RFPROS_FILESHARE_USER):
 SUMMIT_RADIO_URI_BASE          = https://github.com/Ezurio/Connectivity_Stack_Release_Packages/releases/download/LRD-REL
 SUMMIT_RADIO_URI_BASE_INTERNAL = https://$(RFPROS_FILESHARE_AUTH)files.devops.rfpros.com/builds/linux
 
-ifneq ($(SUMMIT_SOM_URI_BASE_ARCHIVE),)
-SUMMIT_RADIO_URI_BASE          = $(SUMMIT_SOM_URI_BASE_ARCHIVE)
-endif
-
 include $(sort $(wildcard $(BR2_EXTERNAL_SUMMIT_RADIO_PATH)/package/*/*.mk))
